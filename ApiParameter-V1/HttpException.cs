e7dd5534fb2e93a956eaff6cfb2005d7dce9285c
Mahyar Mohammadi <devmahyar@hotmail.com>
2022-05-22 12:12:35 +0430

    Make custom UnprocessableEntity excpetion

---
+++ ApiParameter-V1/HttpException.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+
+namespace ApiParameter_V1
+{
+    public class HttpException : Exception
+    {
+        public HttpStatusCode HttpStatusCode
+        {
+            get;
+            private set;
+        }
+
+        public string ErrorCode
+        {
+            get;
+            set;
+        }
+
+        public string Parameter
+        {
+            get;
+            set;
+        }
+
+        public HttpException(HttpStatusCode httpStatusCode , string errorCode ,string message) : this(httpStatusCode,errorCode, message,(string)null)
+        {
+        }
+         
+        public HttpException(HttpStatusCode httpStatusCode,string errorCode , string message, string @params) : base(message)
+        {
+            HttpStatusCode = httpStatusCode;
+            ErrorCode = errorCode;
+            Parameter = @params;
+        }
+    }
+}
---
+++ ApiParameter-V1/UnprocessableEntityException.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiParameter_V1
+{
+    public class UnprocessableEntityException : HttpException
+    {
+        public UnprocessableEntityException() :this( null )
+        {
+        }
+
+        public UnprocessableEntityException(string param) : base((HttpStatusCode)422 , "unprocessable_entity" , "The request was welformed but was unable to be followed due to semantic error",param)
+        {
+        }
+
+        public UnprocessableEntityException(string errorCode , string message) : this(errorCode,message,null)
+        {
+        }
+
+        public UnprocessableEntityException(string errorCode , string message , string param) : base((HttpStatusCode)422 , errorCode, message,param)
+        {
+        }
+    }
+}
