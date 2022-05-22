ce78f1e5debdafc8bda21c5486916396bbd71146
Mahyar Mohammadi <devmahyar@hotmail.com>
2022-05-22 12:14:01 +0430

    Add Parameter interface and implementation

---
+++ ApiParameter-V1/IParameter.cs
@@ -0,0 +1,21 @@
+﻿namespace ApiParameter_V1
+{
+    public interface IParameter<out TValue>
+    {
+        string Name
+        {
+            get;
+        }
+
+        bool HasValue
+        {
+            get; 
+        }
+
+        TValue Value
+        {
+            get; 
+
+        }
+    }
+}
---
+++ ApiParameter-V1/Parameter.cs
@@ -0,0 +1,34 @@
+﻿namespace ApiParameter_V1
+{
+    public class Parameter<TValue> : IParameter<TValue>
+    {
+        private TValue _value;
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public bool HasValue
+        {
+            get;
+            set;
+        }
+
+        public TValue Value
+        {
+            get
+            {
+                if (!this.HasValue)
+                    throw new InvalidOperationException("Parameter must have a value .");
+                return this._value;
+            }
+            set
+            {
+                this._value = value; 
+                this.HasValue = true;
+            }
+        }
+    }
+}
