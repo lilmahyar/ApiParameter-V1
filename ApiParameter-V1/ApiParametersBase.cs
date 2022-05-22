77a0edc94c6aba706d7d3184863852090f2c85ac
Mahyar Mohammadi <devmahyar@hotmail.com>
2022-05-22 11:14:34 +0430

    Add ApiParameterBase class

---
+++ ApiParameter-V1/ApiParametersBase.cs
@@ -0,0 +1,34 @@
+﻿namespace ApiParameter_V1
+{
+    public class ApiParametersBase
+    {
+        private readonly IDictionary<string,object> _values = (IDictionary<string,object>) new Dictionary<string, object>();
+
+        public object ContentObject
+        {
+            get;
+            internal set; 
+        }
+
+        public IEnumerable<string> Keys => _values.Keys;
+
+        public ApiParametersBase()
+        {
+            
+        }
+
+        public ApiParametersBase(IDictionary<string, object> values)
+        {
+            _values = values;
+        }
+
+        public void AddValue(string key, object value)
+        {
+            if (_values.ContainsKey(key))
+                _values[key] = value;
+            else 
+                _values.Add(key,value);
+        }
+
+    }
+}
