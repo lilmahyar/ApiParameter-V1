523575c891c0df62e5369578ee85647382af6aab
Mahyar Mohammadi <devmahyar@hotmail.com>
2022-05-22 12:10:27 +0430

    Add ApiParametersModelBinder

--- ApiParameter-V1/ModelBinders/ApiParametersModelBinder.cs
+++ ApiParameter-V1/ModelBinders/ApiParametersModelBinder.cs
@@ -1,6 +1,24 @@
-﻿namespace ApiParameter_V1.ModelBinders
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Net.Http.Headers;
+
+namespace ApiParameter_V1.ModelBinders
 {
-    public class ApiParametersModelBinder
+    public class ApiParametersModelBinder : IModelBinder
     {
+        private static readonly IEnumerable<string> RouteDataValueParameters = (IEnumerable<string>)new string[2]
+        {
+            "action",
+            "controller"
+        };
+        public async Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            HttpRequest request;
+            ApiParametersModelBinder parameters;
+            IQueryCollection quertCollection;
+            MediaTypeHeaderValue contentType;
+            RouteValueDictionary routeValues;
+            if(bindingContext.ModelType != typeof(Apiparameters))
+        }
     }
 }
